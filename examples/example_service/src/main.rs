f647fb3433f0f42e4e70955e401773816fb0135a
Dirk Leifeld <leifeld@posteo.de>
2019-12-18 19:57:59 +0100

    add example app

---
+++ examples/example_service/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::thread;
+use std::time;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let wait : u64 = args.get(1).unwrap_or(&"2".to_string()).parse().unwrap_or(2);
+	
+    println!("start waiting {} seconds", wait);
+	
+	let seconds = time::Duration::from_secs(wait);
+    thread::sleep(seconds);
+
+    println!("end waiting");
+}
